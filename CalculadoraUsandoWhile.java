c3f026360a310572b5d5cc7c9ed66fc25a2de8db
Roberto <robertoabad93@gmail.com>
2021-01-28 13:00:46 +0100

    se crea una calculadora de multiplos de 5 del 10 al 90 sin mostrar extremos

---
+++ CalculadoraUsandoWhile.java
@@ -0,0 +1,13 @@
+/**
+ * Vamos a crear una claculadora
+ */
+public class CalculadoraUsandoWhile {
+    
+    public void multiplosCinco(){
+        int index = 15;
+        while (index <= 85){
+            System.out.println(index);
+            index = index +5;
+        }
+    }
+}
