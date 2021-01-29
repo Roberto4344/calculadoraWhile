8c0c8a618bdcac3430828ba83895311c574a13b1
Roberto <robertoabad93@gmail.com>
2021-01-29 13:13:00 +0100

    se crea un nuevo metodo para sumar

--- CalculadoraUsandoWhile.java
+++ CalculadoraUsandoWhile.java
@@ -2,12 +2,47 @@
  * Vamos a crear una claculadora
  */
 public class CalculadoraUsandoWhile {
-    
+
     public void multiplosCinco(){
         int index = 15;
-        while (index <= 85){
+        while (index <= 90){
             System.out.println(index);
             index = index +5;
         }
+    }
+
+    public void sumaDeValores(){
+        int index = 0;
+        while (index <= 10){
+            System.out.println(index);
+            index++;
+        }
+    }
+
+    public int sumaDeValoresEnElIntervalo(int a, int b){
+        int suma = 0;
+        if (a == b){
+            suma = b; 
+        }
+        else{
+            if(a < 0 || b < 0){
+                suma= -1;
+            }
+            else{
+                if(a>b){
+                    while (b <= a){                        
+                        suma = suma + b;
+                        b++;
+                    }
+                }
+                else{
+                    while (a <= b){
+                        suma = suma + a;
+                        a++;                        
+                    }
+                }
+            }
+        }
+        return suma;
     }
 }
